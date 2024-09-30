1a76ee4f5abe5570fc2c4ddb405725af19bac055
kaustubhraut-07 <kaustubh.raut@fxis.ai>
2024-09-30 13:01:36 +0530

    ceating todso api done

--- advtodos/todos/urls.py
+++ advtodos/todos/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from .views import getallTodosforUser,createTodo,updateTodo,deleteTodo
 
 urlpatterns = [
-    path('/', ),
+    path('getalltodosbyuserid/<int:id>/', getallTodosforUser , name='getallTodosforUser'),
+    path('createTododo/<int:id>/' ,createTodo , name='createTodo'),
+    path('updatetodo/<int:id>/' ,updateTodo , name='updateTodo'),
+    path('deletetodo/<int:id>/' ,deleteTodo , name='deleteTodo'),
+
 ]
--- advtodos/todos/views.py
+++ advtodos/todos/views.py
@@ -1,18 +1,51 @@
-# myapp/views.py
-from rest_framework import generics
+
 from .models import Todo
 from .serializer import TodoSerializer
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from user.models import CustomUser
 
-class TodoListCreate(generics.ListCreateAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
-    # permission_classes = [IsAuthenticated]
+@api_view(['GET'])
+def getallTodosforUser(request):
+    userId = request.param.get('id')
+    todos = Todo.objects.filter(user=userId)
+    if todos.count() == 0:
+        return Response([], status=status.HTTP_204_NO_CONTENT)
+    else:
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+@api_view(['POST'])
+def createTodo(request,id):
+    try:
+        user = CustomUser.objects.get(id=id) 
+    except CustomUser.DoesNotExist:
+        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+    title = request.data.get('title')
+    description = request.data.get('description')
+    completed = request.data.get('completed')
+    todo = Todo.objects.create(user=user, title=title, description=description, completed=completed)
+    serializer = TodoSerializer(todo)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
-    # permission_classes = [IsAuthenticated]
+@api_view(['PUT'])
+def updateTodo(request):
+    todoId = request.param.get('id')
+    title = request.data.get('title')
+    description = request.data.get('description')
+    completed = request.data.get('completed')
+    todo = Todo.objects.get(id=todoId)
+    todo.title = title
+    todo.description = description
+    todo.completed = completed
+    todo.save()
+    serializer = TodoSerializer(todo)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['DELETE'])
+def deleteTodo(request):
+    todoId = request.param.get('id')
+    todo = Todo.objects.get(id=todoId)
+    todo.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
