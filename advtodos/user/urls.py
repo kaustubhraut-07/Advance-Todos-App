b403083777d0fc859d84ff102a26690573e3c9e4
kaustubhraut-07 <kaustubh.raut@fxis.ai>
2024-09-30 12:15:12 +0530

    login user

--- advtodos/user/urls.py
+++ advtodos/user/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import register
+from .views import register, login
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/', login , name='login'),
 ]
--- advtodos/user/views.py
+++ advtodos/user/views.py
@@ -3,6 +3,9 @@ from .serializers import CustomUserSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate
+
+from .models import CustomUser
 # Create your views here.
 @api_view(['POST'])
 def register(request):
@@ -12,4 +15,23 @@ def register(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['POST'])
+def login(request):
+    if request.method == 'POST':
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return Response({"error": "Username and password are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            serializer = CustomUserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
     
